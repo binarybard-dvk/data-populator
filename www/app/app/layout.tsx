888af5dec4b2057674be6f24fb9577edcdeb8f68
Thoranesh <167609611+binarybard-dvk@users.noreply.github.com>
2024-07-04 10:15:55 +0530

    fix ssr oauth

--- www/app/app/layout.tsx
+++ www/app/app/layout.tsx
@@ -1,17 +1,7 @@
-import { createClient } from "@/lib/supabase";
-import { redirect } from "next/navigation";
-
 export default async function ProtectedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const supabase = createClient();
-
-  const { data, error } = await supabase.auth.getUser();
-  if (error || !data?.user) {
-    redirect("/login");
-  }
-
   return <main className="max-w-2xl mx-auto px-4">{children}</main>;
 }
